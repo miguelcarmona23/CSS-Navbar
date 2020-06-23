169538b37c0003d32c6c49c184570f57f19706b2
Miguel Carmona <miguelcarmona996@gmail.com>
2020-06-23 16:21:16 -0400

    first commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        if ($("button").text() == "☰") {
+            $("button").text("🞬");
+        } else {
+            $("button").text("☰");
+        }
+
+        $("li").toggle("slow");
+    });
+});
